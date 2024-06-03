fad2d9b3596e64113bf72c03e64e785684491d82
Davide Gabrielli <me@davegabe.it>
2024-06-03 22:58:13 +0000

    Fixed deployment script

--- services/set_envs.sh
+++ services/set_envs.sh
@@ -5,17 +5,26 @@
 # aws_secret_access_key=<secret_key>
 # aws_session_token=<session_token>
 echo "Enter the content of ~/.aws/credentials file"
-read input_string
+read input_string_1
+read input_string_2
+read input_string_3
+read input_string_4
 
 # Write the content to ~/.aws/credentials file
 rm -f ~/.aws/credentials
-echo "$input_string" > ~/.aws/credentials
+echo "$input_string_1
+$input_string_2
+$input_string_3
+$input_string_4" > ~/.aws/credentials
 
 # Extract variables from the text
-aws_access_key_id=$(echo "$input_string" | grep -oP '(?<=aws_access_key_id=).*')
-aws_secret_access_key=$(echo "$input_string" | grep -oP '(?<=aws_secret_access_key=).*')
-aws_session_token=$(echo "$input_string" | grep -oP '(?<=aws_session_token=).*')
+AWS_ACCESS_KEY_ID=$(echo "$input_string_2" | grep -oP '(?<=aws_access_key_id=).*')
+AWS_SECRET_ACCESS_KEY=$(echo "$input_string_3" | grep -oP '(?<=aws_secret_access_key=).*')
+AWS_SESSION_TOKEN=$(echo "$input_string_4" | grep -oP '(?<=aws_session_token=).*')
 
+
+# Set default region
+AWS_DEFAULT_REGION="us-east-1"
 
 # Check if the environment variables are set
 if [ -z "$AWS_ACCESS_KEY_ID" ] || [ -z "$AWS_SECRET_ACCESS_KEY" ] || [ -z "$AWS_SESSION_TOKEN" ] || [ -z "$AWS_DEFAULT_REGION" ]; then
@@ -23,8 +32,6 @@ if [ -z "$AWS_ACCESS_KEY_ID" ] || [ -z "$AWS_SECRET_ACCESS_KEY" ] || [ -z "$AWS_
     exit 1
 fi
 
-# Set default region
-AWS_DEFAULT_REGION="us-east-1"
 
 # Write .env file with environment variables
 rm -f .env
@@ -48,3 +55,9 @@ data:
   AWS_SECRET_ACCESS_KEY: $AWS_SECRET_ACCESS_KEY
   AWS_SESSION_TOKEN: $AWS_SESSION_TOKEN
   AWS_DEFAULT_REGION: $AWS_DEFAULT_REGION" > eks-secrets.yml
+
+
+# Enter name of the cluster
+echo "Enter the name of the cluster"
+read CLUSTER_NAME
+aws eks update-kubeconfig --region $AWS_DEFAULT_REGION --name $CLUSTER_NAME
